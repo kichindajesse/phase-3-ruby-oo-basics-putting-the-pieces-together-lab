a40a42eb85623bde1e7458fdccd2a20926481432
kichinda <53686544+kichinda@users.noreply.github.com>
2023-02-10 14:42:37 +0300

    completed

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,22 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
+
+sneaker = Shoe.new("Nike")
+sneaker.color = "Red"
+sneaker.size = 9
+sneaker.material = "Leather"
+sneaker.condition = "worn"
+sneaker.cobble
+puts sneaker.condition
+
--- spec/01_book_spec.rb
+++ spec/01_book_spec.rb
@@ -36,3 +36,50 @@ describe 'Book' do
     end
   end
 end
+
+class Book
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def author=(author)
+    @author = author
+  end
+
+  def page_count=(page_count)
+    @page_count = page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre
+    @genre
+  end
+
+  def author
+    @author
+  end
+end
+
+book = Book.new("And Then There Were None")
+book.page_count = 272
+book.genre = "Mystery"
+book.author = "Agatha Christie"
+book.turn_page
+puts book.title
+
+
